d6ddb35d6b6df9d545b1a89fd6d32837fabc0f4c
david.nzube.official22 <david.nzube.official22@gmail.com>
2025-03-19 00:13:12 +0000

    /6666

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,5 @@
-// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
-// /**
-//  * Metro configuration
-//  * https://reactnative.dev/docs/metro
-//  *
-//  * @type {import('@react-native/metro-config').MetroConfig}
-//  */
-// const config = {};
+const config = getDefaultConfig(__dirname);
 
-// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-
-
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    assetExts: getDefaultConfig(__dirname).resolver.assetExts.filter(ext => ext !== 'svg'),
-    sourceExts: [...getDefaultConfig(__dirname).resolver.sourceExts, 'svg'],
-  },
-});
+module.exports = config;
